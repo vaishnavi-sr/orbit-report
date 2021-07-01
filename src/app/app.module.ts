fda816ac6a0301181b292a5821cd039c1f93cda3
vaishnavi <vaishu.srini15@gmail.com>
2021-07-01 06:48:16 -0500

    step2 & step3-orbit-list files initiated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { OrbitListComponent } from './orbit-list/orbit-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    OrbitListComponent,
     
   ],
   imports: [
